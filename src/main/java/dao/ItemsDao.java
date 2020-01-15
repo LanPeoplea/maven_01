477f8b6a1847dd3412137ab3dd7e3c98ea40cc82
Lanpeople <2368582476@qq.com>
2020-01-15 20:28:53 +0800

    init maven_01 project 20200115

---
+++ src/main/java/dao/ItemsDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import domain.Items;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ItemsDao {
+    public List<Items> findAll() throws Exception;
+
+}
---
+++ src/main/java/domain/Items.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Items {
+    private Integer id;
+    private String name;
+    private Integer num;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+}
